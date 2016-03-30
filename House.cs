16fdf858556df6b7b1a9c273c0a6985cea809298
Nomis <scdockweiler@gmail.com>
2016-03-30 15:24:00 +0200

    House stuff

---
+++ House.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class House : MonoBehaviour {
+
+	ArrayList humans = new ArrayList();
+	[Tooltip("Chance to get out")]
+	public int ctgo = 500;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		if (Random.Range (0, ctgo) < 1) {
+			humans[Random.Range(0,humans.Count)]
+		}
+	}
+
+	void OnTriggerEnter(Collider2D col) {
+		if (col.gameObject.GetComponent<Human> () != null) {
+			col.enabled = false;
+			humans.Add(col.gameObject);
+		}
+	}
+}
