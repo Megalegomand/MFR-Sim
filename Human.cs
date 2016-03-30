b1f5b169fbd1fb784cb8d23d47fb2ef341666464
JJNoura <bohbihbohbih@gmail.com>
2016-03-30 15:49:15 +0200

    Human Fixed

--- Human.cs
+++ Human.cs
@@ -2,13 +2,22 @@
 using System.Collections;
 
 public class Human : MonoBehaviour {
+    float rnd = 0;
+    const int lim = 100;
+    public bool vulnerable;
+    public bool vaccinated;
+    public bool sick;
 
-	// Use this for initialization
-	void Start () {
-        float rnd = Random.Range(0,100);
-        bool vulnerable = rnd <= 20;
-        bool vaccinated = rnd > 20 && rnd >= 80;
-        bool sick = rnd > 80;
+    public Human(){
+        rnd = Random.Range(0, lim);
+        vulnerable = rnd <= (lim - 1 / 5 * lim);
+        vaccinated = rnd > (lim - 1 / 5 * lim) && rnd >= (lim - 4 / 5 * lim);
+        sick = rnd > (lim - 4 / 5 * lim);
+    }
+    // Use this for initialization
+    void Start() {
+        Debug.Log("Vulnerable = ");
+        Debug.Log(vulnerable);
 	}
 	
 	// Update is called once per frame
