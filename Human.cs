c4dc8c7da855611371518231b3c8a8520a386d95
JJNoura <bohbihbohbih@gmail.com>
2016-03-30 15:20:00 +0200

    Human modified

--- Human.cs
+++ Human.cs
@@ -5,7 +5,10 @@ public class Human : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+        float rnd = Random.Range(0,100);
+        bool vulnerable = rnd <= 20;
+        bool vaccinated = rnd > 20 && rnd >= 80;
+        bool sick = rnd > 80;
 	}
 	
 	// Update is called once per frame
