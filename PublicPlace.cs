3d57cfa688a18e395cebf35fee67f3fc3f740a9f
David Lolck <david@lolck.com>
2016-03-30 16:07:51 +0200

    Delition of enum

--- PublicPlace.cs
+++ PublicPlace.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PublicPlace : MonoBehaviour {
+public class PublicPlace : House {
 
 	// Use this for initialization
 	void Start () {
-	
+
+	    ctgo = 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        base.Update();
 	}
 }
