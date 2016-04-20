f34861822c8661b21870489d8ef98beb26ee830e
MnomisC <scdockweiler@gmail.com>
2016-04-20 23:14:00 +0200

    Added coments

--- Background.cs
+++ Background.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 public class Background : MonoBehaviour {
 
 	public static int n = 4;
+
+    // Liste med forbindelser
 	public static List<int>[] points = new List<int>[n];
 	public static GameObject[] gms = new GameObject[n];
     public static List<int> houses = new List<int>();
@@ -15,7 +17,10 @@ public class Background : MonoBehaviour {
 
 	static List<List<int>> paths = new List<List<int>>();
 
+    // Liste af punkter der er besøgt
 	static int[] vis = new int[n];
+    
+    // Nummeret som bliver givet til punkterne (L i flowdiagrammet)
 	static int num = 0;
 
 	// Use this for initialization
@@ -87,43 +92,67 @@ public class Background : MonoBehaviour {
 		points [p2].Add (p1);
 	}
 
+    // Funktionen der kobler dfs og findPath sammen
 	public static List<int> gps(int p1, int p2) {
-		for (int i = 0; i < n; i++) {
+        // Dette loop restarter listen af besøgte steder
+		for (int i = 0; i < vis.Length; i++) {
 			vis[i] = -1;
 		}
+        // Numeret som punkterne for sættes til 0 (L i flowdiagrammet)
 		num = 0;
+        // Der bliver kørt en dybde først søgning fra punktet p2 (B i flowdiagrammet)
 		dfs (p2);
+        // Herefter køres findPath der finder en vej fra p1 (A) til p2 (B)
 		return findPath (p1);
 	}
 
+    // Dybde Først Søgning fra punktet k (B i flowdiagrammet)
 	static void dfs(int k) {
+        // Check at punktet ikke har været besøgt, hvis det har så stop funktionen
 		if (vis[k] != -1) {
 			return;
 		}
+        // Læg 1 til L
 		num++;
+        // Giv det nuværende punkt et nummer
 		vis[k] = num;
+        // Kør igennem alle punkterne omkring det nuværende punkt
 		foreach (int w in points[k]) {
+            // Kør en Dybde Først Søgning fra alle punkter omkring det nuværende punkt
 			dfs (w);
 		}
 	}
 
+    // Find en vej fra p1/v (A) til p2 (B)
 	static List<int> findPath(int v) {
+        // Hvis punktet man prøver at komme hen til ikke er besøgt
+        // så er der ikke en vej til punktet
+        // Derfor returneres en tom liste
 		if (vis [v] == -1) {
 			return new List<int> ();
 		}
+        // Hvis punktet har tallet 1 så er punktet distinationen
+        // Vejen til punktet er derfor selve punktet
 		if (vis[v] == 1) {
 			List<int> k = new List<int>();
 			k.Add(v);
 			return k;
 		}
+        // Gå igennem alle punkter omkring punktet
 		foreach (int w in points[v]) {
+            // Hvis de nye punkt har en mindre værdi en det nuværende køres koden
 			if (vis[w] < vis[v]) {
+                // Der laves en tom liste
 				List<int> k = new List<int>();
+                // Det nuværende punkt tilføjes
 				k.Add(v);
+                // Vejen videre fra dette punkt tilføjes
 				k.AddRange(findPath(w));
+                // Dette returneres
 				return k;
 			}
 		}
+        // Hvis alt går galt så returner ingenting
 		return null;
 	}
 }
